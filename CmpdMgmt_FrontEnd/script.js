13225f64762763612331f44fd400ba7cda5f187a
Luke Nam <lukenamnam@gmail.com>
2019-02-17 02:50:32 -0800

    updated html pages and added javascript file

---
+++ CmpdMgmt_FrontEnd/script.js
@@ -0,0 +1,24 @@
+//Clicking chemical picture will take user to chemical page
+$("#chemicalPicture").click(function(){
+	window.location = "chemical.html";
+});
+
+//Clicking container picture will take user to container page
+$("#containerPicture").click(function(){
+	window.location = "container.html";
+});
+
+//Clicking container type picture will take user to container type page
+$("#containerTypePicture").click(function(){
+	window.location = "containerType.html";
+});
+
+//Clicking rack picture will take user to rack page
+$("#rackPicture").click(function(){
+	window.location = "rack.html";
+});
+
+//Clicking rack type picture will take user to rack type page
+$("#rackTypePicture").click(function(){
+	window.location = "rackType.html";
+});
